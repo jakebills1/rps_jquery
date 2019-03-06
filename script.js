f5a93f3db04a03afa4e86376bb5783acda9eaec6
Jake Bills <jakebills1@gmail.com>
2019-03-06 15:23:53 -0700

    add scoreboard

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready( function() {
+  let wins = 0;
+  let losses = 0;
   $(".button").on("click", function() {
     let choice = this.parentElement.id
     compChoice(choice);
@@ -16,9 +18,13 @@ $(document).ready( function() {
             winner("Results: Tie.")
             break;
           case "paper":
+            losses += 1
+            $("#loseDisp").text("Losses: " + losses)
             winner("Results: You lose.")
             break;
           case "scissors":
+            wins += 1
+            $("#winDisp").text("Wins: " + wins)
             winner("Results: You win!")
             break;
         }
@@ -26,12 +32,16 @@ $(document).ready( function() {
       case "paper":
         switch (c) {
           case "rock":
+            wins += 1;
+            $("#winDisp").text("Wins: " + wins)
             winner("Result: You win!")
             break;
           case "paper":
             winner("Result: Tie.")
             break;
           case "scissors":
+            losses += 1
+            $("#loseDisp").text("Losses: " + losses)
             winner("Result: You lose.")
             break;
         }
@@ -39,9 +49,13 @@ $(document).ready( function() {
       case "scissors":
         switch (c) {
           case "rock":
+            losses += 1
+            $("#loseDisp").text("Losses: " + losses)
             winner("Result: You lose.")
             break;
           case "paper":
+            wins += 1
+            $("#winDisp").text("Wins: " + wins)
             winner("Result: You win!")
             break;
           case "scissors":
